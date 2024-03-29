cf8583c0c4483282324412a4a36c5ce6dfbafc2a
agnesmdev <agnesm.dev@gmail.com>
2024-03-29 14:38:22 +0100

    Add loader

---
+++ app/loading.tsx
@@ -0,0 +1,6 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div className="h-full w-full flex justify-center items-center loading-background">
+    <span className="loader"></span>
+  </div>
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,21 @@ import "./globals.scss";
 import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
 import Background from "@/app/background";
+import Loading from "@/app/loading";
 
 export default function MyApp({Component, pageProps}: AppProps) {
   const router = useRouter();
   const [lang, setLang] = useState("fr");
   const [darkMode, setDarkMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLang(navigator.language.toLocaleLowerCase());
     setDarkMode(window.matchMedia("(prefers-color-scheme: dark)").matches);
+    setLoading(false);
   }, [router.pathname])
 
-  return <>
+  return loading ? <Loading/> : <>
     <Background lang={lang} darkMode={darkMode}/>
     <Component lang={lang} setLang={setLang} darkMode={darkMode} setDarkMode={setDarkMode} {...pageProps} />
   </>
